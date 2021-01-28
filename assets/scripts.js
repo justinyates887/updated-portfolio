28ecde725ef780ddff31e26c048dc6435b404f9d
Justin Yates <justinyates887@gmail.com>
2021-01-28 13:13:59 -0800

    initial commit

---
+++ assets/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    /*Sidebar scripts*/
+    $("#sidebarCollapse").on('click', function(){
+        $("#sidebar").toggleClass('active');
+    });
+    /*End sidebar scripts*/
+
+
+
+});
